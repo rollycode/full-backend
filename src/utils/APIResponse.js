9116d2ffbca4c5eeec04a8ed43d6357a1e4eff33
rollycode <rollymishra@gmail.com>
2024-09-01 07:57:24 +0530

    api error fix

--- src/utils/APIResponse.js
+++ src/utils/APIResponse.js
@@ -5,13 +5,15 @@ class ApiResponse {
     this.statusCode = statusCode;
     this.data = data;
     this.message = message;
-    this.success= 
+    this.success < 400;
   }
 }
 
 //server status code ranges std.
 // 100-199=> information response
-// 200-299=>Success response 
+// 200-299=>Success response
 // 300-399=> redirection response
-// 400-499=>client error 
-// 500-599=> server error 
+// 400-499=>client error
+// 500-599=> server error
+
+export { ApiResponse };
